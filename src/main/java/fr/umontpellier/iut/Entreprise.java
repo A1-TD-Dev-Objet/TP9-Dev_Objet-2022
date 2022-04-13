35b96fb6131c9e1389f100c32cf95049aa0dc946
Odiardo Lois <lois.odiardo@etu.umontpellier.fr>
2022-04-13 16:56:55 +0200

    Exercice 1

--- src/main/java/fr/umontpellier/iut/Entreprise.java
+++ src/main/java/fr/umontpellier/iut/Entreprise.java
@@ -1,18 +1,25 @@
 package fr.umontpellier.iut;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Entreprise {
     private double bonusTotal;
     private Collection<Employe> lePersonnel;
 
+    public Entreprise(){
+        this.lePersonnel= new ArrayList<>();
+    }
+
     public void embaucher(Employe e, LocalDate dateEmbauche) {
-        throw new RuntimeException("Méthode à implémenter");
+        e.setDateEmbauche(dateEmbauche);
+        lePersonnel.add(e);
     }
 
     public void licencier(Employe e) {
-        throw new RuntimeException("Méthode à implémenter");
+        e.setDateEmbauche(null);
+        lePersonnel.remove(e);
     }
 
     public Collection<Employe> getEmployesOrdonnes() {
@@ -37,5 +44,15 @@ public class Entreprise {
 
     public void remercier(int n) {
         throw new RuntimeException("Méthode à implémenter");
+    }
+
+    public String toString(){
+        ArrayList<Employe> personnel = new ArrayList<>();
+        personnel.addAll(lePersonnel);
+        String string = "";
+        for (Employe employe:personnel){
+            string += employe.toString();
+        }
+        return string;
     }
 }
--- src/main/java/fr/umontpellier/iut/GestionEmployes.java
+++ src/main/java/fr/umontpellier/iut/GestionEmployes.java
@@ -1,9 +1,25 @@
 package fr.umontpellier.iut;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class GestionEmployes {
 
     public static void main(String[] args) {
-        throw new RuntimeException("Méthode à implémenter");
+        Employe employe1 = new Employe("uncode","test1",10);
+        Employe employe2 = new Employe("uncode","test2",100);
+        Employe employe3 = new Employe("uncode","test3",101);
+        Employe employe4 = new Employe("uncode","test1",10);
+
+
+        Entreprise entreprise = new Entreprise();
+        entreprise.embaucher(employe1,LocalDate.now());
+        entreprise.embaucher(employe2,LocalDate.of(2000,Month.APRIL,01));
+        entreprise.embaucher(employe3,LocalDate.of(2003,Month.JANUARY,04));
+        entreprise.embaucher(employe4,LocalDate.now());
+
+
+        System.out.println(entreprise.toString());
     }
 
 }
